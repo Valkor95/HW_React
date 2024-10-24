e9a44a378dd85dec6b440af1393866d275d69303
valeriy korovin <1k9o9r5@gmail.com>
2024-10-25 00:13:44 +0300

    HW_React_25.1_v2_'add grid and icons'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
-import {Col, Container, Image, Row} from "react-bootstrap";
+import {Button, Col, Container, Image, Row} from "react-bootstrap";
+
 
 class App extends Component {
+
     render() {
         return (
             <Container fluid className='m-5' >
@@ -10,28 +12,51 @@ class App extends Component {
                         <h3>Голосування за найкращий смайлик</h3>
                     </Col>
                 </Row>
-                <Row className='d-flex justify-content-center column-gap-3'>
-                    <Col xs="auto" className='text-center'>
+                <Row className='d-flex justify-content-center column-gap-3 mb-4'>
+                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
                         <Image src={'../publick/img/angry.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
-                        <p>Vote: 1</p>
+                        <p>Votes: 1</p>
+                        <Button variant='success'>Vote</Button>
                     </Col>
-                    <Col xs="auto" className='text-center'>
+                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
                         <Image src={'../publick/img/love.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
                         <p>Vote: 1</p>
+                        <Button variant='success'>Vote</Button>
                     </Col>
-                    <Col xs="auto" className='text-center'>
+                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
                         <Image src={'../publick/img/sad.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
                         <p>Vote: 1</p>
+                        <Button variant='success'>Vote</Button>
                     </Col>
-                    <Col xs="auto" className='text-center'>
+                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
                         <Image src={'../publick/img/smile.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
                         <p>Vote: 1</p>
+                        <Button variant='success'>Vote</Button>
                     </Col>
-                    <Col xs="auto" className='text-center'>
+                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
                         <Image src={'../publick/img/thinking.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
                         <p>Vote: 1</p>
+                        <Button variant='success'>Vote</Button>
                     </Col>
 
+                </Row>
+
+                <Row className='d-flex justify-content-center mb-3'>
+                    <Col xs='auto'>
+                        <Button variant='primary'>Show results</Button>
+                    </Col>
+                </Row>
+
+                <Row className='d-flex justify-content-center mb-3'>
+                    <Col xs='auto'>
+                        <h3>Результати голосування:</h3>
+                    </Col>
+                </Row>
+
+                <Row className='d-flex justify-content-center mb-3'>
+                    <Col xs='auto'>
+
+                    </Col>
                 </Row>
             </Container>
         );
