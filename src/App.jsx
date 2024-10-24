9b5b65386666da812e23072462b6fbe0035cf365
valeriy korovin <1k9o9r5@gmail.com>
2024-10-25 00:07:52 +0300

    HW_React_25.1_v1_'add grid and icons'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Col, Container, Image, Row} from "react-bootstrap";
 
-const App = () => {
-    return (
-        <div>
-            Hello
-        </div>
-    );
-};
+class App extends Component {
+    render() {
+        return (
+            <Container fluid className='m-5' >
+                <Row className='d-flex justify-content-center mb-3'>
+                    <Col xs='auto'>
+                        <h3>Голосування за найкращий смайлик</h3>
+                    </Col>
+                </Row>
+                <Row className='d-flex justify-content-center column-gap-3'>
+                    <Col xs="auto" className='text-center'>
+                        <Image src={'../publick/img/angry.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
+                        <p>Vote: 1</p>
+                    </Col>
+                    <Col xs="auto" className='text-center'>
+                        <Image src={'../publick/img/love.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
+                        <p>Vote: 1</p>
+                    </Col>
+                    <Col xs="auto" className='text-center'>
+                        <Image src={'../publick/img/sad.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
+                        <p>Vote: 1</p>
+                    </Col>
+                    <Col xs="auto" className='text-center'>
+                        <Image src={'../publick/img/smile.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
+                        <p>Vote: 1</p>
+                    </Col>
+                    <Col xs="auto" className='text-center'>
+                        <Image src={'../publick/img/thinking.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
+                        <p>Vote: 1</p>
+                    </Col>
+
+                </Row>
+            </Container>
+        );
+    }
+}
 
 export default App;
