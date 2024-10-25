e9b7128ec52203a950ce98ced2ba7c01bd63618b
valeriy korovin <1k9o9r5@gmail.com>
2024-10-25 15:41:01 +0300

    HW_React_25.1_v3_'add logic votes'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
 import {Button, Col, Container, Image, Row} from "react-bootstrap";
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            votes: {
+                angry: JSON.parse(localStorage.getItem('angryVotes'))|| 0,
+                love: JSON.parse(localStorage.getItem('loveVotes'))|| 0,
+                sad: JSON.parse(localStorage.getItem('sadVotes'))|| 0,
+                smile: JSON.parse(localStorage.getItem('smileVotes'))|| 0,
+                thinking: JSON.parse(localStorage.getItem('thinkingVotes'))|| 0,
+            },
+            showResults: false
+        };
+    }
+
+    handleVote = (emoji) => {
+        this.setState(prevstate => {
+            const newVotes = {
+                ...prevstate.votes,
+                [emoji]: prevstate.votes[emoji] + 1
+            };
+            localStorage.setItem(`${emoji}Votes`, JSON.stringify(newVotes[emoji]));
+            return {votes: newVotes};
+        })
+    }
+
+    handleShowResults = () => {
+        this.setState({showResults: true});
+    }
+
+    getWinnerEmoji = () => {
+        const {votes} = this.state;
+        const maxVotes = Math.max(...Object.values(votes));
+        const winner = Object.keys(votes).find(key => votes[key] === maxVotes);
+        return winner;
+    }
 
     render() {
+        const {votes, showResults} = this.state;
+        const emojiMap = {
+            angry: 'angry.png',
+            love: 'love.png',
+            sad: 'sad.png',
+            smile: 'smile.png',
+            thinking: 'thinking.png'
+        };
+
         return (
             <Container fluid className='m-5' >
                 <Row className='d-flex justify-content-center mb-3'>
@@ -13,51 +57,32 @@ class App extends Component {
                     </Col>
                 </Row>
                 <Row className='d-flex justify-content-center column-gap-3 mb-4'>
-                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
-                        <Image src={'../publick/img/angry.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
-                        <p>Votes: 1</p>
-                        <Button variant='success'>Vote</Button>
-                    </Col>
-                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
-                        <Image src={'../publick/img/love.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
-                        <p>Vote: 1</p>
-                        <Button variant='success'>Vote</Button>
-                    </Col>
-                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
-                        <Image src={'../publick/img/sad.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
-                        <p>Vote: 1</p>
-                        <Button variant='success'>Vote</Button>
-                    </Col>
-                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
-                        <Image src={'../publick/img/smile.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
-                        <p>Vote: 1</p>
-                        <Button variant='success'>Vote</Button>
-                    </Col>
-                    <Col xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
-                        <Image src={'../publick/img/thinking.png'} rounded alt='angry' style={{width: '50px', height: '50px'}}/>
-                        <p>Vote: 1</p>
-                        <Button variant='success'>Vote</Button>
-                    </Col>
+                    {Object.keys(votes).map((emoji, index) => (
+                        <Col key={index} xs="auto" className='d-flex align-items-center flex-column row-gap-2'>
+                            <Image src={`../publick/img/${emojiMap[emoji]}`} rounded alt={emoji} style={{width: '50px', height: '50px'}}/>
+                            <p>Votes: {votes[emoji]}</p>
+                            <Button variant='success' onClick={() => this.handleVote(emoji)}>Vote</Button>
+                        </Col>
+                    ))}
+
 
                 </Row>
 
                 <Row className='d-flex justify-content-center mb-3'>
                     <Col xs='auto'>
-                        <Button variant='primary'>Show results</Button>
+                        <Button variant='primary' onClick={this.handleShowResults}>Show results</Button>
                     </Col>
                 </Row>
 
-                <Row className='d-flex justify-content-center mb-3'>
-                    <Col xs='auto'>
-                        <h3>Результати голосування:</h3>
-                    </Col>
-                </Row>
-
-                <Row className='d-flex justify-content-center mb-3'>
-                    <Col xs='auto'>
-
-                    </Col>
-                </Row>
+                {showResults && (
+                    <Row className='d-flex justify-content-center mb-3'>
+                        <Col xs='auto'>
+                            <h3>Результати голосування:</h3>
+                            <Image src={`../public/img/${emojiMap[this.getWinnerEmoji()]}`} rounded alt='winner' style={{width: '50px', height: '50px'}}/>
+                            <p>Переможець: {this.getWinnerEmoji()}</p>
+                        </Col>
+                    </Row>
+                )}
             </Container>
         );
     }
