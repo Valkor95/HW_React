7d6d654818d085a7c6aa8b282df537193abedf20
valeriy korovin <1k9o9r5@gmail.com>
2024-11-01 16:34:30 +0200

    HW_React_27.1_v4_'add AppBar header'

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,3 @@
-// import {BottomNavigation, Button, Stack} from "@mui/material";
-
-// function Header(props) {
-//     return (
-//         <div>
-//             <Stack direction="row" alignItems='center' justifyContent='center' >
-//                 <BottomNavigation label='1'/>
-//                 <BottomNavigation label='2'/>
-//                 <BottomNavigation label='3'/>
-//             </Stack>
-//         </div>
-//     );
-// }
-
-// export default Header;
-
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
@@ -22,23 +6,26 @@ import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import {NavLink} from "react-router-dom";
+import {AppBar} from "@mui/material";
 
 export default function SimpleBottomNavigation() {
     const [value, setValue] = React.useState(0);
 
     return (
-        <Box sx={{ width: 500 }}>
-            <BottomNavigation
-                showLabels
-                value={value}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                }}
-            >
-                <BottomNavigationAction component={NavLink} to='/home' label="Головна" icon={<RestoreIcon />}/>
-                <BottomNavigationAction component={NavLink} to='/contacts' label="Контакти" icon={<FavoriteIcon />}/>
-                <BottomNavigationAction component={NavLink} to='/about' label="Про мене" icon={<LocationOnIcon />}/>
-            </BottomNavigation>
-        </Box>
+        <AppBar>
+            <Box sx={{ width: 500 }}>
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                >
+                    <BottomNavigationAction component={NavLink} to='/home' label="Головна" icon={<RestoreIcon />} />
+                    <BottomNavigationAction component={NavLink} to='/contacts' label="Контакти" icon={<LocationOnIcon />}/>
+                    <BottomNavigationAction component={NavLink} to='/about' label="Про мене" icon={<LocationOnIcon />}/>
+                </BottomNavigation>
+            </Box>
+        </AppBar>
     );
 }
