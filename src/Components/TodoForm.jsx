61d276ebc4d5ad9214ce1b27c0035d3657cfdb03
valeriy korovin <1k9o9r5@gmail.com>
2024-11-02 02:36:54 +0200

    HW_React_27.1_v8_'add todoform logic'

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -1,11 +1,21 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Button, TextField} from "@mui/material";
 import './style/MainPage.css'
-function TodoForm(props) {
-    const [todoValue, setTodoValue] = useState({})
+function TodoForm() {
+    const input = useRef()
 
     const submit = (event) => {
         event.preventDefault();
+        const todoText = input.current.value.trim()
+        if (todoText === '') return;
+
+        const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+
+        const newTodo = {id: Date.now(), text: todoText}
+
+        localStorage.setItem('todos', JSON.stringify([...savedTodos, newTodo]))
+
+        input.current.value = '';
     }
     return (
         <div>
@@ -17,6 +27,7 @@ function TodoForm(props) {
                     helperText="Запишіть вашу таску"
                     variant="outlined"
                     margin="normal"
+                    inputRef={input}
                 />
                 <Button type='submit' variant='outlined'>Відправити</Button>
             </form>
