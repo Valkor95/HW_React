704ea12aa57de40f5dac75e82e56e264e423fca9
valeriy korovin <1k9o9r5@gmail.com>
2024-11-03 16:39:50 +0200

    HW_React_27.1_v18_'create ThemeProvider'

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import MainPage from "./Components/Pages/MainPage.jsx";
 import ContactsPage from "./Components/Pages/ContactsPage.jsx";
 import AboutPage from "./Components/Pages/AboutPage.jsx";
 import Header from "./Components/Header.jsx";
-import {useState} from "react";
-import {Button, createTheme, ThemeProvider} from "@mui/material";
-import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider as MuiThemeProvider } from "@mui/material";
+import { ThemeProvider, useTheme } from './ThemeContext.js';
 
 function App() {
-
+    const { darkMode } = useTheme();
+    const theme = createTheme({
+        palette: {
+            mode: darkMode ? 'dark' : 'light',
+        },
+    });
     return (
-        <ThemeProvider theme={theme}>
+        <MuiThemeProvider theme={theme}>
             <BrowserRouter>
                 <Header/>
-
                 <Routes>
                     <Route path='/' element={<Navigate to='/home' replace />} />
                     <Route path='/home' element={<MainPage/>}/>
@@ -21,7 +24,7 @@ function App() {
                     <Route path='/about' element={<AboutPage/>}/>
                 </Routes>
             </BrowserRouter>
-        </ThemeProvider>
+        </MuiThemeProvider>
     );
 }
 
