4fffc938b8e0f9cc89cdd92b182a939b6dad71d5
valeriy korovin <1k9o9r5@gmail.com>
2024-11-08 22:51:41 +0200

    HW_React_28.1_v9_'add theme'

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,41 +1,54 @@
 import React from 'react';
-import {AppBar, Box, Container, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Container, createTheme, ThemeProvider, Toolbar, Typography} from "@mui/material";
 import Grid from '@mui/material/Grid2';
 import Header from "./Header.jsx";
 import Routes from "./Routes.jsx";
+import {useTheme} from "../ContextTheme.jsx";
 
 function Layout(props) {
+    const { darkMode } = useTheme();
+
+    const theme = createTheme({
+        palette: {
+            mode: darkMode ? 'dark' : 'light',
+            background: {
+                default: darkMode ? '#303030' : '#ffffff',
+            }
+        },
+    });
     return (
-        <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-            <Header/>
+        <ThemeProvider theme={theme}>
+            <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+                <Header/>
 
-            <Container maxWidth="lg" sx={{ flex: 1, my: 4 }}>
-                <Grid
-                    container
-                    spacing={3}
-                    sx={{ minHeight: '100vh' }}
-                    justifyContent='center'
-                    alignItems="flex-start"
-                >
-                    <Grid size={{ xs: 12, md: 3 }}>
-                        <Box sx={{ bgcolor: 'grey.200', p: 2, textAlign: 'center' }}>SideBar Left</Box>
+                <Container maxWidth="lg" sx={{ flex: 1, my: 4 }}>
+                    <Grid
+                        container
+                        spacing={3}
+                        sx={{ minHeight: '100vh' }}
+                        justifyContent='center'
+                        alignItems="flex-start"
+                    >
+                        <Grid size={{ xs: 12, md: 3 }}>
+                            <Box sx={{ bgcolor: 'grey.200', p: 2, textAlign: 'center' }}>SideBar Left</Box>
+                        </Grid>
+                        <Grid  size={{ xs: 12, md: 6 }}>
+                            <Box sx={{ bgcolor: 'grey.100', p: 2, textAlign: 'center' }}>
+                                <Routes/>
+                            </Box>
+                        </Grid>
+                        <Grid  size={{ xs: 12, md: 3 }}>
+                            <Box sx={{ bgcolor: 'grey.200', p: 2, textAlign: 'center' }}>SideBar Right</Box>
+                        </Grid>
                     </Grid>
-                    <Grid  size={{ xs: 12, md: 6 }}>
-                        <Box sx={{ bgcolor: 'grey.100', p: 2, textAlign: 'center' }}>
-                            <Routes/>
-                        </Box>
-                    </Grid>
-                    <Grid  size={{ xs: 12, md: 3 }}>
-                        <Box sx={{ bgcolor: 'grey.200', p: 2, textAlign: 'center' }}>SideBar Right</Box>
-                    </Grid>
-                </Grid>
-            </Container>
+                </Container>
 
-            {/* Footer */}
-            <Box component="footer" sx={{ bgcolor: 'primary.main', color: 'white', py: 2, textAlign: 'center' }}>
-                <Typography variant="body2">Footer</Typography>
+                {/* Footer */}
+                <Box component="footer" sx={{ bgcolor: 'primary.main', color: 'white', py: 2, textAlign: 'center' }}>
+                    <Typography variant="body2">Footer</Typography>
+                </Box>
             </Box>
-        </Box>
+        </ThemeProvider>
     );
 }
 
