5ff48ca8eea8bfca3163392d4fc3666849d94d2a
valeriy korovin <1k9o9r5@gmail.com>
2024-11-08 23:00:53 +0200

    HW_React_28.1_v10_'add theme toggle'

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import {AppBar, Toolbar, Typography} from "@mui/material";
+import {AppBar, Button, Toolbar, Typography} from "@mui/material";
+import {useTheme} from "../ContextTheme.jsx";
 
 function Header(props) {
+    const {darkMode, toggleTheme} = useTheme()
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Header
                 </Typography>
+                <Button variant='outlined' color={darkMode ? "secondary" : ""} onClick={toggleTheme}>
+                    Включити {darkMode ? 'cвітлу' : 'темну'} тему
+                </Button>
             </Toolbar>
         </AppBar>
     );
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppBar, Box, Container, createTheme, ThemeProvider, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Container, createTheme, CssBaseline, ThemeProvider, Toolbar, Typography} from "@mui/material";
 import Grid from '@mui/material/Grid2';
 import Header from "./Header.jsx";
 import Routes from "./Routes.jsx";
@@ -18,36 +18,38 @@ function Layout(props) {
     });
     return (
         <ThemeProvider theme={theme}>
-            <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-                <Header/>
+            <CssBaseline>
+                <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+                    <Header/>
 
-                <Container maxWidth="lg" sx={{ flex: 1, my: 4 }}>
-                    <Grid
-                        container
-                        spacing={3}
-                        sx={{ minHeight: '100vh' }}
-                        justifyContent='center'
-                        alignItems="flex-start"
-                    >
-                        <Grid size={{ xs: 12, md: 3 }}>
-                            <Box sx={{ bgcolor: 'grey.200', p: 2, textAlign: 'center' }}>SideBar Left</Box>
+                    <Container maxWidth="lg" sx={{ flex: 1, my: 4 }}>
+                        <Grid
+                            container
+                            spacing={3}
+                            sx={{ minHeight: '100vh' }}
+                            justifyContent='center'
+                            alignItems="flex-start"
+                        >
+                            <Grid size={{ xs: 12, md: 3 }}>
+                                <Box sx={{ bgcolor: darkMode ? '#4f4f4f' : 'grey.200', p: 2, textAlign: 'center' }}>SideBar Left</Box>
+                            </Grid>
+                            <Grid  size={{ xs: 12, md: 6 }}>
+                                <Box sx={{ bgcolor: darkMode ? '#4f4f4f' : 'grey.200', p: 2, textAlign: 'center' }}>
+                                    <Routes/>
+                                </Box>
+                            </Grid>
+                            <Grid  size={{ xs: 12, md: 3 }}>
+                                <Box sx={{ bgcolor: darkMode ? '#4f4f4f' : 'grey.200', p: 2, textAlign: 'center' }}>SideBar Right</Box>
+                            </Grid>
                         </Grid>
-                        <Grid  size={{ xs: 12, md: 6 }}>
-                            <Box sx={{ bgcolor: 'grey.100', p: 2, textAlign: 'center' }}>
-                                <Routes/>
-                            </Box>
-                        </Grid>
-                        <Grid  size={{ xs: 12, md: 3 }}>
-                            <Box sx={{ bgcolor: 'grey.200', p: 2, textAlign: 'center' }}>SideBar Right</Box>
-                        </Grid>
-                    </Grid>
-                </Container>
+                    </Container>
 
-                {/* Footer */}
-                <Box component="footer" sx={{ bgcolor: 'primary.main', color: 'white', py: 2, textAlign: 'center' }}>
-                    <Typography variant="body2">Footer</Typography>
+                    <Box component="footer" sx={{ bgcolor: darkMode ? '#4f4f4f' : 'primary.main', color: 'white', py: 2, textAlign: 'center' }}>
+                        <Typography variant="body2">Footer</Typography>
+                    </Box>
                 </Box>
-            </Box>
+            </CssBaseline>
+
         </ThemeProvider>
     );
 }
