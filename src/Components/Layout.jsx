04d596fd3d140ab88442975d4e67cbf2c8892402
valeriy korovin <1k9o9r5@gmail.com>
2024-11-08 14:38:03 +0200

    HW_React_28.1_v5_'fix Grid'

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import {AppBar, Box, Container, Toolbar, Typography} from "@mui/material";
 import Grid from '@mui/material/Grid2';
+import Header from "./Header.jsx";
 
 function Layout(props) {
     return (
         <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-            {/* Header */}
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Header
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+            <Header/>
 
             {/* Main Content */}
             <Container maxWidth="lg" sx={{ flex: 1, my: 4 }}>
-                
+                <Grid container spacing={3}>
+                    <Grid sx={{background: 'red'}} size={{ xs: 12, md: "grow" }}>
+                        <div>size=grow</div>
+                    </Grid>
+                    <Grid sx={{background: 'red'}} size={{ xs: 12, md: 6 }}>
+                        <div>size=6</div>
+                    </Grid>
+                    <Grid sx={{background: 'red'}} size={{ xs: 12, md: "grow" }}>
+                        <div>size=grow</div>
+                    </Grid>
+                </Grid>
             </Container>
 
             {/* Footer */}
