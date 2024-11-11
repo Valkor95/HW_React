3e7d6af8c1129b5da2a025ba83af9b5829cd8b38
valeriy korovin <1k9o9r5@gmail.com>
2024-11-11 17:34:19 +0200

    HW_React_28.1_v35_'add BottomNavigation in Header'

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import {AppBar, Button, Toolbar, Typography} from "@mui/material";
+import React, {useState} from 'react';
+import {AppBar, BottomNavigation, Button, Toolbar, Typography} from "@mui/material";
 import {useTheme} from "../Context.jsx";
 
 function Header(props) {
-    const {darkMode, toggleTheme} = useTheme()
+    const {darkMode, toggleTheme} = useTheme();
+    const [value, setValue] = useState(0);
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Header
                 </Typography>
+                <BottomNavigation
+
+                >
+
+                </BottomNavigation>
                 <Button variant='outlined' color={darkMode ? "secondary" : ""} onClick={toggleTheme}>
                     Включити {darkMode ? 'cвітлу' : 'темну'} тему
                 </Button>
