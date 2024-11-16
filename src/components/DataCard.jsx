f464c7321eba2ffc05e8b73813a3c3933b4581e0
valeriy korovin <1k9o9r5@gmail.com>
2024-11-16 20:38:54 +0200

    HW_React_30.1_v6_'add layout in DataCard'

--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import {Card, CardContent, Typography, useTheme} from "@mui/material";
 
 function DataCard(props) {
+    const theme = useTheme();
     return (
-        <div></div>
+        <Card
+            sx={{
+                width: 300,
+                margin: 'auto',
+                boxShadow: 3,
+                borderRadius: 2,
+                backgroundColor: '#424242',
+                color: theme.palette.text.primary,
+            }}
+        >
+            <CardContent>
+                <Typography variant="h5" component="div" gutterBottom>
+                    Заголовок
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Здесь будет отображаться информация из состояния Redux.
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
 
