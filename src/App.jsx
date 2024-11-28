2fee3acac55677345423b35ec28c9918e44568ca
valeriy korovin <1k9o9r5@gmail.com>
2024-11-28 16:10:56 +0200

    HW_React_31.1_v9_'create app layout'

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,28 @@ const App = () => {
 
     return (
         <div className='App'>
-            Hello
+            <h1>CAT SPECIES GALLERY</h1>
+            <p>Images of different species of cats. Lots of cats for your viewing pleasure.</p>
+            <hr/>
+            <div className='Gallery'>
+                {cats.map( cat =>
+                    <div key={cat.id} className='row'>
+                        <div className='column column-left'>
+                            <img
+                                alt={cat.name}
+                                src={`https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg`}
+                                width='200'
+                                height='200'
+                            />
+                        </div>
+                        <div className='column column-right'>
+                            <h2>{cat.name}</h2>
+                            <h5>Temperament: {cat.temperament}</h5>
+                            <p>{cat.description}</p>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
