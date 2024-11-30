6e6439a374f46d34766155241b4fe162a58c8b4e
valeriy korovin <1k9o9r5@gmail.com>
2024-12-01 00:01:25 +0200

    HW_React_32.1_v12_'create navigation'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ImageComponent from "./components/ImageComponent.jsx";
+import Navigation from "./components/Navigation.jsx";
+import Routers from "./components/Routers.jsx";
 
 const App = () => {
     return (
         <div>
-
+            <Navigation/>
+            <Routers/>
         </div>
     );
 };
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {AppBar, Tab, Tabs, Toolbar} from "@mui/material";
+import {Link} from "react-router-dom";
 
 function Navigation(props) {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    }
+
+    const navLinks = [
+        { label: 'CV', to: '/' },
+        { label: 'Список дел', to: '/todolist' },
+        { label: 'SWAPI', to: '/swapi' },
+    ];
+
     return (
-        <div></div>
+        <AppBar position="static" color="primary">
+            <Toolbar>
+                <Tabs
+                   value={value}
+                   onChange={handleChange}
+                   textColor="inherit"
+                   indicatorColor="secondary"
+                >
+                    {navLinks.map((link, index) => (
+                        <Tab key={index} label={link.label} component={Link} to={link.to}/>
+                    ))}
+                </Tabs>
+            </Toolbar>
+        </AppBar>
     );
 }
 
