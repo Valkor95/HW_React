6289f210db5f829b282e5f1b2db163c56e6d0f4f
valeriy korovin <1k9o9r5@gmail.com>
2024-12-01 01:50:15 +0200

    HW_React_32.1_v18_'download img SW'

--- src/pages/SWAPI.jsx
+++ src/pages/SWAPI.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchImageSWRequest} from "../store/slice/imageSlice.js";
+import {Box} from "@mui/material";
 
 function Swapi(props) {
+    const {urlSW, loading, error } = useSelector((state) => state.image)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchImageSWRequest())
+    }, [dispatch]);
+
     return (
-        <div>
-            SWAPI
-        </div>
+        <>
+            <Box sx={{
+                marginTop: "15px"
+            }}>
+                {urlSW && <img src={urlSW} alt="SW" style={{ width: '600px' }} />}
+            </Box>
+            {loading &&
+                    <h1>Loading...</h1>
+            }
+            {error &&
+                <Box>
+                    <p>Error: {error}</p>
+                </Box>
+            }
+
+        </>
+
     );
 }
 
