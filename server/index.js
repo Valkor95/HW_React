038f4afbac56c01f136bb544e4a453eaea006bf8
valeriy korovin <1k9o9r5@gmail.com>
2024-11-30 11:38:53 +0200

    HW_React_32.1_v1_'create server'

---
+++ server/index.js
@@ -0,0 +1,56 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let users = [];
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the User API');
+});
+
+app.post('/users', (req, res) => {
+    const newUser = { id: Date.now(), ...req.body };
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.get('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const userIndex = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (userIndex === -1) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    users[userIndex] = { id: parseInt(req.params.id), ...req.body };
+    res.json(users[userIndex]);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const userIndex = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (userIndex === -1) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    users.splice(userIndex, 1);
+    res.status(204).send();
+});
+
