71eb557449cfaecab26a853bff3acc9c94a56a1e
valeriy korovin <1k9o9r5@gmail.com>
2024-12-05 00:24:53 +0200

    HW_React_32.1_v20_'add SW data slice to store'

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import imageReducer from './slice/imageSlice.js';
+import fetchSWData from './slice/SWstate.js';
 import rootSaga from './saga/rootSaga.js';
+
 
 
 const saga = createSagaMiddleware();
@@ -9,6 +11,7 @@ const saga = createSagaMiddleware();
 const store = configureStore({
     reducer: {
         image: imageReducer,
+        SWData: fetchSWData,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saga)
 })
