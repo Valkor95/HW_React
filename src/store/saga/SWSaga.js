1d24cbecd8aa12c67137a877a138981c0e165b36
valeriy korovin <1k9o9r5@gmail.com>
2024-12-05 00:43:01 +0200

    HW_React_32.1_v22_'create SW Saga'

--- src/store/saga/SWSaga.js
+++ src/store/saga/SWSaga.js
@@ -4,3 +4,21 @@ import {fetchSWDataRequest, fetchSWDataSuccess, fetchSWDataFailure} from '../sli
 
 const HOST = 'https://swapi.dev/api/'
 
+function* fetchDataSaga(url, successAction){
+    try {
+        const response = yield call(axios.get, `${HOST}${url}`);
+        yield put(successAction(response.data.results));
+    } catch (e){
+        yield put(fetchSWDataFailure(e.message));
+    }
+}
+
+function* fetchDataPlanetsSaga(){
+    yield fetchDataSaga('planets', fetchSWDataSuccess)
+}
+
+export function* watchFetchSWSaga(){
+    yield all([
+        takeLatest(fetchSWDataRequest, fetchDataPlanetsSaga),
+    ])
+}
--- src/store/saga/rootSaga.js
+++ src/store/saga/rootSaga.js
@@ -1,8 +1,10 @@
 import {all} from 'redux-saga/effects';
 import {watchFetchImageSaga} from './imageSaga.js'
+import {watchFetchSWSaga} from "./SWSaga.js";
 
 export default function* rootSaga(){
     yield all([
         watchFetchImageSaga(),
+        watchFetchSWSaga()
     ])
 }
