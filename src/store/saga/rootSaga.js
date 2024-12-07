499506dc799e740dc4003c1526af0a430e493176
valeriy korovin <1k9o9r5@gmail.com>
2024-12-07 18:43:20 +0200

    HW_React_32.1_v29_'create saga todos'

--- src/store/saga/rootSaga.js
+++ src/store/saga/rootSaga.js
@@ -1,10 +1,12 @@
 import {all} from 'redux-saga/effects';
 import {watchFetchImageSaga} from './imageSaga.js'
 import {watchFetchSWSaga} from "./SWSaga.js";
+import {watchFetchTodosSaga} from "./todoSaga.js";
 
 export default function* rootSaga(){
     yield all([
         watchFetchImageSaga(),
-        watchFetchSWSaga()
+        watchFetchSWSaga(),
+        watchFetchTodosSaga(),
     ])
 }
--- src/store/saga/todoSaga.js
+++ src/store/saga/todoSaga.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest, all} from 'redux-saga/effects';
 
 import {
     fetchTodosRequest, fetchTodosSuccess, fetchTodosFailure,
@@ -6,3 +6,40 @@ import {
     deleteTodoRequest, deleteTodoSuccess, deleteTodoFailure
 } from '../slice/todosSlice.js'
 
+import { getTodos, updateTodo, deleteTodo } from '../api/api.js'
+import {fetchImageCVRequest, fetchImageSWRequest} from "../slice/imageSlice.js";
+
+function* fetchTodosSaga() {
+    try {
+        const todos = yield call(getTodos);
+        yield put(fetchTodosSuccess(todos));
+    } catch (error) {
+        yield put(fetchTodosFailure(error.message));
+    }
+}
+
+function* updateTodoSaga(action) {
+    try {
+        const updatedTodo = yield call(updateTodo, action.payload);
+        yield put(updateTodoSuccess(updatedTodo));
+    } catch (error) {
+        yield put(updateTodoFailure(error.message));
+    }
+}
+
+function* deleteTodoSaga(action) {
+    try {
+        yield call(deleteTodo, action.payload);
+        yield put(deleteTodoSuccess(action.payload));
+    } catch (error) {
+        yield put(deleteTodoFailure(error.message));
+    }
+}
+
+export function* watchFetchTodosSaga(){
+    yield all([
+        takeLatest(fetchTodosRequest.type, fetchTodosSaga),
+        takeLatest(updateTodoRequest.type, updateTodoSaga),
+        takeLatest(deleteTodoRequest.type, deleteTodoSaga),
+    ])
+}
