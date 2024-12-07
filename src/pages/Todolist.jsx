4d84089a99b4487210263ffad7c41750ea5b9dfb
valeriy korovin <1k9o9r5@gmail.com>
2024-12-07 18:56:12 +0200

    HW_React_32.1_v30_'create page todolist'

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchTodosRequest} from "../store/slice/todosSlice.js";
+import {CircularProgress, Typography} from "@mui/material";
+import Grid from '@mui/material/Grid2';
 
 function Todolist(props) {
+    const dispatch = useDispatch();
+    const { todos, loading, error } = useSelector(state => state.todos);
+
+    useEffect(() => {
+        dispatch(fetchTodosRequest());
+    }, [dispatch]);
+
+    if (loading) return <CircularProgress />;
+    if (error) return <Typography color="error">Ошибка: {error}</Typography>;
+
     return (
-        <div>
-            Todolist
-        </div>
+        <Grid container spacing={3} sx={{ p: 3 }}>
+            <Grid item xs={12}>
+                <Typography variant="h4" gutterBottom>Todo List</Typography>
+            </Grid>
+
+            <Grid item xs={12}>
+                <TodoForm />
+            </Grid>
+
+            <Grid item xs={12}>
+                <Typography variant="h5" gutterBottom>Список задач</Typography>
+            </Grid>
+
+            {todos.map(todo => (
+                <Grid item xs={12} sm={6} md={4} key={todo.id}>
+                    <TodoItem todo={todo} />
+                </Grid>
+            ))}
+        </Grid>
     );
 }
 
